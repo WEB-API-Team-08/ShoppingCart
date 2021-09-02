d3217f9a949297a2b40a22e9d21c5818fbf1746e
Lakshancgit <chamika.ratnasiri@gmail.com>
2021-09-02 15:14:35 +0530

    finalize_Auth-017

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,33 +1,35 @@
-
-
-
+const { Router } = require("express");
+const User = require("../models/user");
+const { userValidation } = require("../validations/userValidation");
+const router = Router();
 
 
 // POST
 router.post("/", async (req, res) => {
-    const token = req.header("x-jwt-token");
-    if (!token) {
-      return res.status(401).send("Access denied. No token available");
+
+  //Validate the data before we create a link
+  const { error } = userValidation(req.body);
+  if (error) return res.status(400).send({ message: error.details[0].message });
+
+  const { name, sub } = req.body;
+
+  try {
+    let user = await User.find({ sub: sub });
+    if (!user.length) {
+      user = new User({
+        name: name,
+        sub: sub,
+      });
+
+      user = await user.save();
+      return res.send(user);
     }
-    try {
-      jwt.verify(token, SECRET_KEY);
-    } catch (e) {
-      return res.status(400).send("Invalid token");
-    }
-  
-    if (!req.body.name) {
-      return res.status(400).send("Not all mandatory values are sent");
-    }
-  
-    let bear = new Bear({
-      name: req.body.name,
-      type: req.body.type,
-      movies: req.body.movies,
-      likeCount: req.body.likeCount,
-      imgUrl: req.body.imgUrl,
-      isScary: req.body.isScary,
-    });
-  
-    bear = await bear.save();
-    res.send(bear);
-  });
+    return res.status(200).send("Login successful");
+
+  } catch (e) {
+    return res.status(500).send("Database/Server Error");
+  }
+
+});
+
+module.exports = router;
